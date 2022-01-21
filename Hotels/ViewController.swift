ddb91e230681a2e76db3d8896c9d13451666e34c
Andrey Alymov <dupodupovich@gmail.com>
2022-01-21 13:55:08 +0300

    Initial Commit

---
+++ Hotels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotels
+//
+//  Created by Andrey Alymov on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
